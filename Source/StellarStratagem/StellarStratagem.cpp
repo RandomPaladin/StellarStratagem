f112c7a6da4d029493aba04189a54e8066e70611
RandomPaladin <The_Rstar@hotmail.com>
2022-06-18 08:07:56 -0600

    Initial commit

---
+++ Source/StellarStratagem/StellarStratagem.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StellarStratagem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StellarStratagem, "StellarStratagem" );
---
+++ Source/StellarStratagem/StellarStratagemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StellarStratagemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STELLARSTRATAGEM_API AStellarStratagemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
