8526170a306836d65399a425551ad7efa26832e0
Nicholas Mondino <nicholasmondino2003@gmail.com>
2025-10-13 17:24:54 +0200

    Removed some test code

--- Source/StellarStratagem/Multiplayer/OnlineGameInstance.h
+++ Source/StellarStratagem/Multiplayer/OnlineGameInstance.h
@@ -2,7 +2,6 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
-#include "Interfaces/OnlineSessionInterface.h"
 #include "OnlineGameInstance.generated.h"
 
 class IOnlineSubsystem;
@@ -12,28 +11,10 @@ class STELLARSTRATAGEM_API UOnlineGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
-	IOnlineSubsystem* OnlineSub;
-	IOnlineSessionPtr SessionInterface;
-
-	FName TestSessionName = "TestSession";
-
-	TSharedPtr<FOnlineSessionSearch> SessionSearchSettings;
-
-	FOnDestroySessionCompleteDelegate OnDestroyOldSessionCompleteDelegate;
-	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
-	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
-
 	UPROPERTY(EditAnywhere)
 	TSoftObjectPtr<UWorld> LobbyMap;
 
-	void EnsureSetup();
-
-	void OnDestroyOldSessionComplete(FName SessionName, bool Successful);
-	void OnFindSessionsComplete(bool Successful);
-
 public:
 	UFUNCTION(BlueprintCallable)
-	void CreateSession();
-	UFUNCTION(BlueprintCallable)
-	void FindSessions();
+	void ConnectToServer();
 };
