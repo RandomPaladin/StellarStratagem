f073af111cd8016c529e5b76d19f514af0e9d431
Nicholas Mondino <nicholasmondino2003@gmail.com>
2025-11-18 16:39:16 +0100

    Updated ship attack line visual

--- Source/StellarStratagem/Gameplay/ShipAttackLine.cpp
+++ Source/StellarStratagem/Gameplay/ShipAttackLine.cpp
@@ -4,21 +4,11 @@
 AShipAttackLine::AShipAttackLine()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
-	RootComp = CreateDefaultSubobject<USceneComponent>("RootSceneComp");
-	RootComponent = RootComp;
-
-	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("AttackLineMesh");
-	StaticMeshComp->SetupAttachment(RootComp);
 }
 
 void AShipAttackLine::SetTargetLoc(const FVector& Loc) const
 {
-	const FVector StartToTargetVec = Loc - GetActorLocation();
-	float Dist = StartToTargetVec.Size();
-
-	StaticMeshComp->SetRelativeRotation(StartToTargetVec.ToOrientationRotator());
-	StaticMeshComp->SetRelativeScale3D({Dist, 1.f, 1.f});
+	OnShipAttackLineTargetLocUpdated.Broadcast(Loc);
 }
 
 void AShipAttackLine::SetupAttackLine(AStellarPlayerController* InPlayer, APlanet* InFromPlanet, APlanet* InTargetPlanet)
